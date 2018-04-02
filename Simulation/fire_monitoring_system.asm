c232cabf898e6d44e2336a1eb7ddb11cb6e844d3
Jacob John <kapsjacob@gmail.com>
2018-04-03 00:33:49 +0530

    added simulation

---
+++ Simulation/fire_monitoring_system.asm
@@ -0,0 +1,6 @@
+;Emulate a fire monitoring system on emu8086 for the following specifications:
+;Define the threshold for the temperature of two rooms
+;Generate the temperature value in 8b resolution
+;Switch on the alarm and display an alarm message when the threshold of either of the room is reached
+;Remove the alarm and bring the temperature below the threshold
+
