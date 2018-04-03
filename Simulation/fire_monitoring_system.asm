58585cda460f2f3cb8c7bd2aaf4bc408ac4ac75b
Jacob John <kapsjacob@gmail.com>
2018-04-03 09:56:06 +0530

    completed

--- Simulation/fire_monitoring_system.asm
+++ Simulation/fire_monitoring_system.asm
@@ -5,19 +5,20 @@
 ;Remove the alarm and bring the temperature below the threshold
 
 DATA SEGMENT
-    msg1 db 10,13,"Define threshold for room 1: $"
-    msg2 db 10,13,"Define threshold for room 2: $"
-    msg3 db 10,13,"Alarm switched on. $"
-    msg4 db 10,13,"Alarm! Threshold reached! $"
-    msg5 db 10,13,"1 = Restart, anything else = Exit"
     num1 db ?
     num2 db ?
+    MSG1 DB 10,13,"Threshold for 1: $"
+    MSG2 DB 10,13,"Threshold for 2: $"
+    MSG3 DB 10,13,"Alarm on! $"
+    MSG4 DB 10,13,"Threshold reached! $"
+    MSG5 DB 10,13,"Press '1' to restart. $"
+    MSG6 DB 10,13,"Temp: $"
 DATA ENDS
 
 CODE SEGMENT
     ASSUME DS:DATA,CS:CODE
     
-START: mov AX,@data ;intialize data segment
+START: mov AX,data  ;intialize data segment
        mov DS,AX
        
        lea dx,msg1  ;load and display message 1
@@ -40,7 +41,17 @@ START: mov AX,@data ;intialize data segment
 
  l0:   mov cl,0h    ;initial temperature at 0
 
- l1:   inc cl       ;temperature of the room increases
+ l1:   lea dx,msg6  ;load and display message 6
+       mov ah,9h
+       int 21h
+ 
+       add cl,30h   ;ASCII adjust before displaying
+       mov dl,cl
+       mov ah,2h    ;display it
+       int 21h
+       sub cl,30h   ;ASCII adjust after display
+       
+       inc cl       ;temperature of the room increases
        cmp cl,num1  ;check against first threshold
        jge re
 
@@ -53,7 +64,11 @@ re:    lea dx,msg3  ;load and display alarm is on
        mov ah,9h
        int 21h
 
-       lea dx,msg4  ;load and display restart
+       lea dx,msg4  ;load and display threshold
+       mov ah,9h
+       int 21h
+       
+       lea dx,msg5  ;load and display restart
        mov ah,9h
        int 21h
 
