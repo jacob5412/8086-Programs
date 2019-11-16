27ce82aa77cdecddc0be533480395c0359021bba
Jacob John <kapsjacob@gmail.com>
2019-11-16 19:44:12 +0530

    Update 36_daa.asm

--- Arithmetic/36_daa.asm
+++ Arithmetic/36_daa.asm
@@ -1,2 +1,14 @@
-org 100h
+.MODEL SMALL
+.STACK 200
+.DATA
+NUM1	DB 	27H
+NUM2	DB	35H	
+.CODE
+.STARTUP
 
+	MOV	AL,  NUM1	;load AX with number NUM1
+	ADD 	AL,  NUM2	;AL = AL + NUM2 i.e. AL = 5CH = 92 in decimal
+	;The expected result is 62 in decimal
+	DAA			; AL = 62
+.EXIT
+END
