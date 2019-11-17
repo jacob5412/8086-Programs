23fb6b5d393d4f2104d04ce20684bd816ddc99d0
Jacob <kapsjacob@gmail.com>
2019-11-17 11:55:50 +0530

    updated reverse number

--- Arithmetic/36_daa.asm
+++ Arithmetic/36_daa.asm
@@ -1,14 +1,14 @@
 .MODEL SMALL
 .STACK 200
 .DATA
-NUM1	DB 	27H
-NUM2	DB	35H	
+NUM1 DB	27H
+NUM2 DB	35H	
+
 .CODE
 .STARTUP
-
-	MOV	AL,  NUM1	;load AX with number NUM1
-	ADD 	AL,  NUM2	;AL = AL + NUM2 i.e. AL = 5CH = 92 in decimal
+	MOV	AL, NUM1	;load AX with number NUM1
+	ADD AL, NUM2	;AL = AL + NUM2 i.e. AL = 5CH = 92 in decimal
 	;The expected result is 62 in decimal
-	DAA			; AL = 62
+	DAA				; AL = 62
 .EXIT
 END
